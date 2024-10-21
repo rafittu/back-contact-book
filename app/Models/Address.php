5a23ccf3869ff7f537a97c0979e9b442cfe79e57
rafittu <rafittu@outlook.com>
2024-10-21 17:47:21 -0300

    feat: address model

---
+++ app/Models/Address.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Ramsey\Uuid\Uuid;
+
+class Address extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['cep', 'street', 'neighborhood', 'city', 'state'];
+
+    public static function boot()
+    {
+        parent::boot();
+
+        self::creating(function ($model) {
+            $model->id = Uuid::uuid4()->toString();
+        });
+    }
+}
+
